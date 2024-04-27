3f045c86cdb9a5434e6c93fb0209f7fa7aaaf877
Amit Singh <amitds1997@gmail.com>
2024-04-27 22:56:57 +0530

    refactor(constants): make constant cap-case

--- lua/remote-nvim/constants.lua
+++ lua/remote-nvim/constants.lua
@@ -3,7 +3,7 @@
 ---@alias plenary.logger table Plenary logger instance
 
 return {
-  version = "0.0.1", -- x-release-please-version
+  PLUGIN_VERSION = "0.0.1", -- x-release-please-version
   PLUGIN_NAME = "remote-nvim.nvim",
   MIN_NEOVIM_VERSION = "v0.9.0",
 }
